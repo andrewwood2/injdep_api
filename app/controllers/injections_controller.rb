7fef7621673efa6f128974b8a6a8630eab691c62
Andrew Wood <andrew.wood.dev@outlook.com>
2018-10-23 18:24:44 +0100

    Feat: able to return a users injection data

--- app/controllers/injections_controller.rb
+++ app/controllers/injections_controller.rb
@@ -6,6 +6,13 @@ class InjectionsController < ApplicationController
     @injection.save
   end
 
+  def index
+    return nil if !params[:user_id]
+    @injections = Injection.where("user_id = '#{params[:user_id]}'")
+    p @injections
+    render json: @injections
+  end
+
   private
   def injection_params
     params.require(:injection).permit(:user_id, :site, :time, :medtype)
