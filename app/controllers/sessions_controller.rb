48b2a78d1a1ba8f09c2a00dce726ba4fef3986cf
Andrew Wood <andrew.wood.dev@outlook.com>
2018-11-14 12:10:09 +0000

    Feat: Change the error number and message on failed login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < Devise::SessionsController
+  def create
+    user = User.find_by_username(sign_in_params[:username])
+
+    if user && user.valid_password?(sign_in_params[:password])
+      @current_user = user
+    else
+      render json: { errors: { 'username or password' => ['is invalid'] } }, status: :unprocessable_entity
+    end
+  end
+end
