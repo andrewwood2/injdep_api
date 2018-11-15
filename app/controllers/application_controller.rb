6d2d95a553115a151d7cd0a91dcc7b13a3232610
Andrew Wood <andrew.wood.dev@outlook.com>
2018-11-15 11:56:50 +0000

    Feat: Authentication tokens seemingly working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::API
 
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
   respond_to :json
-  before_action :underscore_params!
+  # before_action :underscore_params!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user
 
   private
-  def underscore_params!
-    params.deep_transform_keys!(&:underscore)
-  end
+  # def underscore_params!
+  #   params.deep_transform_keys!(&:underscore)
+  # end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
@@ -17,14 +17,18 @@ class ApplicationController < ActionController::API
 
   def authenticate_user
     if request.headers['Authorization'].present?
-      authenticate_or_request_with_http_token do |token|
-        begin
-          jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
-
-          @current_user_id = jwt_payload['id']
-        rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
-          head :unauthorized
-        end
+      token = JSON.parse(request.headers['Authorization'])
+      # authenticate_or_request_with_http_token do |token|
+      begin
+        jwt_payload = JWT.decode(
+          token,
+          Rails.application.secret_key_base,
+          true,
+          { algorithm: 'HS256' }
+        ).first
+        @current_user_id = jwt_payload['id']
+      rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+        head :unauthorized
       end
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'jwt'
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -9,7 +11,8 @@ class User < ApplicationRecord
   def generate_jwt
     JWT.encode(
       { id: id, exp: 60.days.from_now.to_i },
-      Rails.application.secrets.secret_key_base
+      Rails.application.secret_key_base,
+      'HS256'
     )
   end
 end
